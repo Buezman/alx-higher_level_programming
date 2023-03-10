f9de5e4eb8082aaf6c03d55dd87601364946a66d
buezman <chibuezenwajiobi@gmail.com>
2023-03-09 18:50:01 -0800

    add 102-magic_calculation.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,13 +11,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.arg[1])
-    b = int(sys.arg[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-
-    if sys.argv[2] in list(ops.keys()):
-        print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    if sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)

11c7eec95058be70b3a7a6c0836bfd930f5e52ff
buezman <chibuezenwajiobi@gmail.com>
2023-03-19 21:53:11 -0700

    add 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    result = []
+
+    for x in my_list:
+        if (x == search):
+            result.append(replace)
+        else:
+            result.append(x)
+
+    return result

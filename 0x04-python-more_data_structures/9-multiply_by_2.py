2aa6ce12886896a138db996e111b6c38a0ab4212
buezman <chibuezenwajiobi@gmail.com>
2023-03-20 21:40:14 -0700

    add 9-nultiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
+    new_dict = {}
+
     for k in a_dictionary:
         val = a_dictionary[k] * 2
-        a_dictionary.update({k: val})
+        new_dict.update({k: val})
+
+    return new_dict

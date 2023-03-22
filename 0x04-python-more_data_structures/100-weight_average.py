8f9a4506eb3759cd791bb569163a3ea7020ce418
buezman <chibuezenwajiobi@gmail.com>
2023-03-22 08:28:40 -0700

    add 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if (not isinstance(my_list, list) or len(my_list) == 0):
+        return 0
+
+    sum_weight = 0
+    sum_score = 0
+
+    for i in range(len(my_list)):
+        score = my_list[i][0]
+        weight = my_list[i][1]
+
+        sum_weight += score * weigth
+        sum_score += weight
+
+    return (sum_score / sum_weight)

db68f865bd1970204b187e62e76984d210b75954
buezman <chibuezenwajiobi@gmail.com>
2023-04-11 19:42:04 -0700

    add 3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 3-is_kind_of_class.py
+"""Describes a function to validate an instance or inherited class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if obj is an instance of or inherits a_class
+    
+    Args:
+        obj (any): object to be checked
+        a_class (type): class to be checked against
+    Returns:
+        If obj is instance or inherits from a_class - True
+        Otherwise - False
+    """
+    return (isinstance(obj, a_class))

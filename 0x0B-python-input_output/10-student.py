abc5f93b0733080b626ab50712369161dc8cf048
buezman <chibuezenwajiobi@gmail.com>
2023-04-12 21:32:15 -0700

    add 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# 10-student.py
+"""Defines a student class"""
+
+
+class Student:
+    """Student class representation"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiates a new Student.
+
+        Args:
+            first_name (str): studemt's firstname
+            last_name (str): studen'ts lastname
+            age (int): student's age
+        """
+        self.first_name = first_name
+        self.lastname = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """Returns a dictionary representation of a student object
+
+        Args:
+            attr (list): list of attributes
+        """
+        if (type(attrs) == list and
+                all(type(att) == str for att in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__

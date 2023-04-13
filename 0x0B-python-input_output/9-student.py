17f5281e2b10ca9474ef725da5dec2f0ff9b05c7
buezman <chibuezenwajiobi@gmail.com>
2023-04-12 21:25:57 -0700

    add 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# 9-student.py
+"""Defines a student class"""
+
+
+class Student:
+    """Student class representation"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiates a new Student.
+
+        Args:
+            first_name (str): studemt's firstname
+            last_name (str): studen'ts lastname
+            age (int): student's age
+        """
+        self.first_name = first_name
+        self.lastname = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns a dictionary representation of a student object"""
+        return self.__dict__

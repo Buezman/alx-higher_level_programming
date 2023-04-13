b269d5333a5a24afc756c3dbf040ad2b439e7e8a
buezman <chibuezenwajiobi@gmail.com>
2023-04-12 21:46:08 -0700

    add 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# 11-student.py
+"""Defines a student class"""
+
+
+class Student:
+    """Student class representation"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiates a new Student.
+
+        Args:
+            first_name (str): studemt's firstname
+            last_name (str): studen'ts lastname
+            age (int): student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of a student object
+
+        Args:
+            attr (list): list of attributes
+        """
+        if (type(attrs) == list and all(type(att) == str for att in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)

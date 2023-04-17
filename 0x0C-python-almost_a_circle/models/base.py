33e0d5e9a7cebf67366f2a8523de43b1220ac40b
buezman <chibuezenwajiobi@gmail.com>
2023-04-17 02:13:06 -0700

    add base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# base.py
+"""Base class"""
+
+
+class Base:
+    """Base class for project.
+
+    Attributes:
+        __nb_objects (int): number of instantiated Bases
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialize a new base object.
+
+        Args:
+            id (int): id of new base
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

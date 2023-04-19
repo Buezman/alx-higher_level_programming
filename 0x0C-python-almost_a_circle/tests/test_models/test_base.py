fff088414172be91a114e294a1aa96aa90a2f58d
buezman <chibuezenwajiobi@gmail.com>
2023-04-18 20:01:54 -0700

    add unit tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -21,6 +21,10 @@ from models.square import Square
 class TestBase_instantiation(unittest.TestCase):
     """Unit tests for testing instantiation of the Base class."""
 
+    def test_auto_id(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
     def test_no_arg(self):
         b1 = Base()
         b2 = Base()

8c2f7b794891b28320c6cc0c61fad9e6d96dbd75
buezman <chibuezenwajiobi@gmail.com>
2023-06-02 09:54:00 -0700

    add 101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Lists average temperatures of cities ordered by temperature
 SELECT city, AVG(value) AS avg_temp
-FROM TABLE temperatures
+FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;

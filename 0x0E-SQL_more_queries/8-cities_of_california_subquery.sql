8c084e736dddc7b9b66f7cd391f89ac4cf5dc6b6
buezman <chibuezenwajiobi@gmail.com>
2023-06-03 15:07:24 -0700

    add 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Lists all cities of California in hbtn_0d_usa database
-SELECT * FROM hbtn_0d_usa.cities WHERE state_id IN (
-	SELECT id FROM hbtn_0d_usa.states WHERE name = 'California'
+SELECT * FROM hbtn_0d_usa.cities c WHERE c.state_id IN (
+	SELECT id FROM hbtn_0d_usa.states s WHERE s.name = 'California'
 ) AS cali_ids;

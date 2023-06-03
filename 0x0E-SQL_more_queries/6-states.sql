c24e14abf6d9349cad373bdd27fd7396f04d7e97
buezman <chibuezenwajiobi@gmail.com>
2023-06-03 14:24:44 -0700

    add 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- Creates new database hbtn_0d_usa and new table 'states'
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-USE hbtn_0d_usa;
-CREATE TABLE `states` IF NOT EXISTS (
+CREATE TABLE hbtn_0d_usa.states IF NOT EXISTS (
 	id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
 	name VARCHAR(256)
 );

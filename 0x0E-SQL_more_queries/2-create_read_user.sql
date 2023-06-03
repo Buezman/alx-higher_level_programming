4c8800dc1e2a928ec12673a2935b5c5060f70d19
buezman <chibuezenwajiobi@gmail.com>
2023-06-03 13:48:11 -0700

    add 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates database hbtn_0d_2 and user_0d_2
 -- and adds SELECT privilege to user_0d_2
-CREATE DATABASE IF NOT EXISTS hbyn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;

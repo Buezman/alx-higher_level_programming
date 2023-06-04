f6b9773ed90f599077e9fd80ecfc2084f97804e7
buezman <chibuezenwajiobi@gmail.com>
2023-06-04 12:28:31 -0700

    add 101-not_a_comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,9 @@
 -- Lists all non-comedy shows in the database
 SELECT s.title FROM tv_shows s
-JOIN tv_show_genres sg
-ON (s.id = sg.show_id)
-JOIN tv_genres g
-ON (g.id = sg.genre_id)
-WHERE g.name <> 'Comedy'
+WHERE s.id NOT IN (
+	SELECT sg.show_id FROM tv_show_genres sg
+	JOIN tv_genres g
+	ON (sg.genre_id = g.id)
+	WHERE g.name = 'Comedy'
+	)
 ORDER BY s.title;

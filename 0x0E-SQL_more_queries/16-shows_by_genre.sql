d70d58d1d28af4e0f614330319910de6bf8c1d17
buezman <chibuezenwajiobi@gmail.com>
2023-06-04 12:02:47 -0700

    add 16-shows_by_genres.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- List Shows by genre
+SELECT s.title, g.name
+FROM tv_shows s
+LEFT JOIN tv_show_genres sg
+ON (s.id = sg.show_id)
+LEFT JOIN tv_genres g
+ON (g.id = sg.genre_id)
+ORDER BY s.title, g.name;

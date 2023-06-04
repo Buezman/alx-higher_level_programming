61b008b39ef7c6fc32a6817655fcd50783db5369
buezman <chibuezenwajiobi@gmail.com>
2023-06-04 12:47:15 -0700

    add 103-rating_genres.sql

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- Lists all genres according to their ratings
+SELECT g.name, SUM(r.rate)
+FROM tv_genres g
+JOIN tv_show_genres sg
+ON (g.id = sg.genre_id)
+JOIN tv_show_ratings r
+ON (r.show_id = sg.show_id)
+GROUP BY g.name
+ORDER BY g.name DESC;

d20a87ca2be9e78d820ed8ec12c7045d74584cef
buezman <chibuezenwajiobi@gmail.com>
2023-06-04 11:41:22 -0700

    add 13-count_shows_by_henre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,5 @@ SELECT g.name AS genre, COUNT(sg.id) AS number_of_shows
 FROM tv_genres g
 JOIN tv_show_genres sg
 ON (g.id = sg.genre_id)
-WHERE number_of_shows > 0
-GROUP BY genre
+GROUP BY sg.id
 ORDER BY number_of_shows DESC;

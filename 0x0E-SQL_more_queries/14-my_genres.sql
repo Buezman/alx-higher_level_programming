0a255d2331ed6e023ff6db12766b57001592b99e
buezman <chibuezenwajiobi@gmail.com>
2023-06-04 11:51:57 -0700

    add 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- Lists all genres of the show Dexter
 SELECT g.name FROM tv_genres g
-JOIN tv_shows_genres sg
+JOIN tv_show_genres sg
 ON (g.id = sg.genre_id)
 JOIN tv_shows s
 ON (s.id = sg.show_id)

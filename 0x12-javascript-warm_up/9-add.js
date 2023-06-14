2c22a7c9e67b1fe648bdbacd5267a792c37eea2e
buezman <chibuezenwajiobi@gmail.com>
2023-06-14 04:59:22 +0000

    add 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const a = process.argv[2];
-const b = process.argv[3];
+const a = Math.floor(Number(process.argv[2]));
+const b = Math.floor(Number(process.argv[3]));
 function add (a, b) {
   return a + b;
 }
